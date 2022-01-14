0b2ecac3de0169170db03f7e6fdcbb576678b21c
pepvalencia <pepvalencia@users.noreply.github.com>
2022-01-14 08:52:39 +0100

    runnable

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MyUtilities;
 
 namespace MyFirstProgramb
 {
@@ -7,6 +8,8 @@ namespace MyFirstProgramb
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var res= MyUtilities.WeatherUtilities.FahrenheitToCelsius(1f);
+            Console.WriteLine(res);
         }
     }
 }
